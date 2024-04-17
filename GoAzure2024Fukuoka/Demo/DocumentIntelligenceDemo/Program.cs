a399d1986cfb07a166ddbd8ac37cf7f30d4707a3
kosmos.ebi <kosmosebi@azure.moe>
2024-04-18 03:28:37 +0900

    add demo1

---
+++ GoAzure2024Fukuoka/Demo/DocumentIntelligenceDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using Azure;
+using Azure.AI.DocumentIntelligence;
+using Microsoft.Extensions.Configuration;
+
+var config = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
+
+var endpoint = config.GetSection("DocumentIntelligence")["Endpoint"] ?? "";
+var apiKey = config.GetSection("DocumentIntelligence")["ApiKey"] ?? "";
+
+var client = new DocumentIntelligenceClient(
+            new Uri(endpoint),
+            new AzureKeyCredential(apiKey),
+            new DocumentIntelligenceClientOptions(DocumentIntelligenceClientOptions.ServiceVersion.V2024_02_29_Preview)
+            );
+
+var model = "prebuilt-layout";
+
+Console.Write("input document uri: ");
+var documentUri = Console.ReadLine() ?? "";
+
+// using var file = File.OpenRead(@"fuga.pdf");
+
+var ops = await client.AnalyzeDocumentAsync(
+    WaitUntil.Started,
+    model,
+    analyzeRequest: new AnalyzeDocumentContent() {
+        UrlSource = new Uri(documentUri)
+//        Base64Source = await BinaryData.FromStreamAsync(file)
+    },
+    features:
+    [
+        DocumentAnalysisFeature.KeyValuePairs,
+    ],
+    outputContentFormat: ContentFormat.Markdown,
+    cancellationToken: default
+);
+
+var result = await ops.WaitForCompletionAsync();
+
+Console.WriteLine("finished");
