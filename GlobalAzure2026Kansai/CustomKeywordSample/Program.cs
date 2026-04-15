8b24869547a877998ce23b386b8977c8b4748575
kosmos.ebi <kosmosebi@azure.moe>
2026-04-16 02:10:42 +0900

    add custom keyword sample

---
+++ GlobalAzure2026Kansai/CustomKeywordSample/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+
+var keyword = "hey kosmos";
+
+using var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+using var keywordRecognizer = new KeywordRecognizer(audioConfig);
+var keywordModel = KeywordRecognitionModel.FromFile("hey_kosmos.table");
+
+Console.WriteLine($"\"{keyword}\" が検出されるまで待ちます..");
+
+await keywordRecognizer.RecognizeOnceAsync(keywordModel);
+
+Console.WriteLine($"\"{keyword}\" を検出しました (enterで終了)");
+Console.ReadKey();
+
+await keywordRecognizer.StopRecognitionAsync();
