7e50b93ffca62f3ee7f9401bd7a37d5329b5377a
kosmos.ebi <kosmosebi@azure.moe>
2026-05-10 01:47:41 +0900

    support post refinement for STT with translate

--- GlobalAzure2026Kansai/TranslateSample/Program.cs
+++ GlobalAzure2026Kansai/TranslateSample/Program.cs
@@ -11,6 +11,9 @@ config.SpeechRecognitionLanguage = "ja-JP"; // 元音声の言語
 config.AddTargetLanguage("en"); // 翻訳先の言語
 config.SpeechSynthesisVoiceName = "ja-jp-Nanami:DragonHDLatestNeural";
 
+// use post refinement (2pass)
+config.SetProperty(PropertyId.SpeechServiceResponse_PostProcessingOption, "PostRefinement");
+
 using var translationRecognizer = new TranslationRecognizer(config, audioConfig);
 var stopRecognition = new TaskCompletionSource<int>(TaskCreationOptions.RunContinuationsAsynchronously);
 
@@ -19,10 +22,10 @@ using var speechSynthesizer = new SpeechSynthesizer(config, audioOutputConfig);
 
 translationRecognizer.Recognizing += (s, e) =>
 {
-    Console.WriteLine($"\tRECOGNIZING: {e.Result.Text}");
+    Console.WriteLine($"\t{DateTimeOffset.Now:HH:mm.ss} RECOGNIZING: {e.Result.Text}");
     foreach (var (language, translation) in e.Result.Translations)
     {
-        Console.WriteLine($"\tTRANSLATING into '{language}': {translation}");
+        Console.WriteLine($"\t{DateTimeOffset.Now:HH:mm.ss} TRANSLATING into '{language}': {translation}");
     }
 };
 
@@ -30,15 +33,15 @@ translationRecognizer.Recognized += (s, e) =>
 {
     if (e.Result.Reason == ResultReason.RecognizedSpeech)
     {
-        Console.WriteLine($"RECOGNIZED: {e.Result.Text}");
+        Console.WriteLine($"{DateTimeOffset.Now:HH:mm.ss} RECOGNIZED: {e.Result.Text}");
         stopRecognition.TrySetResult(0);
     }
     else if (e.Result.Reason == ResultReason.TranslatedSpeech)
     {
-        Console.WriteLine($"RECOGNIZED: {e.Result.Text}");
+        Console.WriteLine($"{DateTimeOffset.Now:HH:mm.ss} RECOGNIZED: {e.Result.Text}");
         foreach (var (language, translation) in e.Result.Translations)
         {
-            Console.WriteLine($"TRANSLATED into '{language}': {translation}");
+            Console.WriteLine($"{DateTimeOffset.Now:HH:mm.ss} TRANSLATED into '{language}': {translation}");
             _ = speechSynthesizer.SpeakTextAsync(translation);
         }
     }
